db644e3c4ed311b9449105ea2c57674f85696d5c
Matheus_Santos_ <matheus.antonio@ufu.br>
2021-03-05 00:09:41 -0300

    changes

--- lista 3/ex4L3.c
+++ lista 3/ex4L3.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 
+   typedef struct alunos{
+       float nota;
+       char name; 
+    }alunos;
 
 int main(){
-   int i;
-   float nota[3],maior,menor;
-   char name[3];
+   alunos  al[3];
+   int i, j=0;
+   float maior,menor;
+   char nMaior, nMenor;
     printf("Nota da Turma\n");
     for(i=1;i<4;i++){
-        int j=0;
         printf("Informe a Inicial do Aluno #%d: ", i);
         setbuf(stdin, NULL);
-        scanf("%c", &name[j]);
+        scanf("%c", &al[j].name);
         printf("Informe a Nota: ");
         setbuf(stdin, NULL);
-        scanf("%f", &nota[j]);j++;   
+        scanf("%f", &al[j].nota);j++;   
     }
-    maior = nota[0];
-    menor = nota[0];
+    maior = al[0].nota;
+    menor = al[0].nota;
+    nMaior = al[0].name;
+    nMenor = al[0].name;
     for(i=0;i<3;i++){
-    if(maior<nota[i])maior=nota[i];
+    if(maior<al[i].nota){maior=al[i].nota; nMaior = al[i].name;}
     }
-    printf("maior %.1f\n", maior);
+    for(i=0;i<3;i++){
+    if(menor>al[i].nota){menor=al[i].nota; nMenor = al[i].name;}
+    }
+    
+
+    printf("%c tem a maior nota %.1f e %c tem a pior nota  %.1f\n",nMaior, maior,nMenor, menor);
     
     return 0;
 }
