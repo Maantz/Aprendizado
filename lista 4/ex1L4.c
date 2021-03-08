fa32454ac2fea862d5aa85a89ea9522c6aae84ac
Matheus_Santos_ <matheus.antonio@ufu.br>
2021-03-08 18:46:01 -0300

    changes

---
+++ lista 4/ex1L4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int i=10;
+    printf("<<Contagem regressiva>>\n");
+    while(i>-1){
+        printf("%d...", i);i--;
+    }
+    printf("FIM!");
+    return 0;
+}
