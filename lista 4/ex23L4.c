bc40b864d692142ab77ba40624b70b422b1de45e
Matheus_Santos_ <matheus.antonio@ufu.br>
2021-03-08 19:08:48 -0300

    changes now

--- lista 4/ex23L4.c
+++ lista 4/ex23L4.c
@@ -2,6 +2,8 @@
 
 int main(){
     double fat=1, i, n, e=1;
+    printf("Numero neperiano\n");
+    printf("Digite quantidade de termos: ");
     scanf("%lf", &n);
     for(i=1;i<=n;i++){
         fat*=i;
