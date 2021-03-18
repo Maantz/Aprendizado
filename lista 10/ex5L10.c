47a4e917b6651e3954a11ac682f139e4f035b552
Matheus_Santos_ <matheus.antonio@ufu.br>
2021-03-18 18:26:11 -0300

    changes

--- lista 10/ex5L10.c
+++ lista 10/ex5L10.c
@@ -11,18 +11,19 @@ p_v1 = &v1;
 p_v2 = &v2; 
 p_c = &c;
 for(int j=-2;j<3;j++){
-printf("\nEndereco+%d de v1 %u",j, p_v1+j); 
-printf("\nEndereco+%d de v2 %u",j, p_v2+j); 
-printf("\nEndereco+%d de c %u",j, p_c+j);
+if(j<0){
+    printf("\nEndereco %d de v1 %u",j, p_v1+j); 
+    printf("\nEndereco %d de v2 %u",j, p_v2+j); 
+    printf("\nEndereco %d de c %u",j, p_c+j);
+}else if(j==0){
+    printf("\nEndereco de v1 %u", p_v1); 
+    printf("\nEndereco de v2 %u", p_v2); 
+    printf("\nEndereco de c %u", p_c);
+}else if(j>0){
+    printf("\nEndereco +%d de v1 %u",j, p_v1+j); 
+    printf("\nEndereco +%d de v2 %u",j, p_v2+j); 
+    printf("\nEndereco +%d de c %u",j, p_c+j);
 }
-printf("\n%u", p_v1);
-/*printf("\nEndereco+1 de v1 %u", p_v1+1); 
-printf("\nEndereco+1 de v2 %u", p_v2+1); 
-printf("\nEndereco+1 de c %u", p_c+1);
-printf("\nEndereco-1 de v1 %u", p_v1-1);   
-printf("\nEndereco-1 de v2 %u", p_v2-1);   
-printf("\nEndereco-1 de c %u", p_c-1);
-*/
-
+}
     return 0;
 }
